4f13389869e0ab18774f440544e5eba73b3e476f
Mila-Kostic <milakostic.eng@gmail.com>
2022-05-19 08:16:41 +0200

    if & else

--- while.py
+++ while.py
@@ -1,4 +1,7 @@
 temp = 0
 while temp != -1000:
     temp = eval(input('Unesite temperaturu (-1000 za kraj): '))
-    print('U Faranhajtima to je ', 9/5*temp+32)
+    if temp!=-1000:
+        print('U Faranhajtima to je ', 9/5*temp+32)
+    else:
+        print('Kraj!')

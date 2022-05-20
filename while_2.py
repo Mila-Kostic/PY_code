64d39bb3c8810d1393ae911405e9d28f74ece9cf
Mila-Kostic <milakostic.eng@gmail.com>
2022-05-20 09:00:21 +0200

    if else

--- while_2.py
+++ while_2.py
@@ -1,2 +1,5 @@
-temp = eval(input) 
-// add code
+temp = eval(input('Unesite temperaturu u Celzijusima: ')) 
+if temp < -273.15:
+    print('Nemoguca temperatura.')
+else:
+    print('U Farenhajtima to je', 9/5*temp+32)

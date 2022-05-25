29bcbea31e4a3c15459cbe6b0a40f8201b208d76
Mila-Kostic <milakostic.eng@gmail.com>
2022-05-25 09:22:34 +0200

    break

---
+++ break_for.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    num = eval(input('Unesite broj: '))
+    if num < 0:
+        break
---
+++ while_break.py
@@ -0,0 +1,4 @@
+i = 0
+num = 10
+while i < 10 and num>0: 
+    num = eval(input('Unesite broj: '))
